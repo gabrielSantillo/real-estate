e2c97186d9daa865d209df0993ce29c6a62f3f5e
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-20 11:55:44 -0700

    Adding the tags to the residencies component

--- src/components/ResidenciesComponent.vue
+++ src/components/ResidenciesComponent.vue
@@ -7,6 +7,86 @@
         <h2 class="section__title">
             Popular Residencies
         </h2>
+
+        <div class="popular__container">
+            <div>
+                <article class="popular__card">
+                    <img src="@/img/popular1.jpg" alt="" class="popular__img">
+
+                    <div class="popular__data">
+                        <h2 class="popular__price">
+                            <span>$</span>66,356
+                        </h2>
+                        <h3 class="popular__title">
+                            Garden City Assat
+                        </h3>
+                        <p class="popular__description">
+                            Street The Garden City of Miraflores, Lima - peru Av Sol #9876
+                        </p>
+                    </div>
+                </article>
+                                <article class="popular__card">
+                    <img src="@/img/popular1.jpg" alt="" class="popular__img">
+
+                    <div class="popular__data">
+                        <h2 class="popular__price">
+                            <span>$</span>66,356
+                        </h2>
+                        <h3 class="popular__title">
+                            Garden City Assat
+                        </h3>
+                        <p class="popular__description">
+                            Street The Garden City of Miraflores, Lima - peru Av Sol #9876
+                        </p>
+                    </div>
+                </article>
+                <article class="popular__card">
+                    <img src="@/img/popular1.jpg" alt="" class="popular__img">
+
+                    <div class="popular__data">
+                        <h2 class="popular__price">
+                            <span>$</span>66,356
+                        </h2>
+                        <h3 class="popular__title">
+                            Garden City Assat
+                        </h3>
+                        <p class="popular__description">
+                            Street The Garden City of Miraflores, Lima - peru Av Sol #9876
+                        </p>
+                    </div>
+                </article>
+                <article class="popular__card">
+                    <img src="@/img/popular1.jpg" alt="" class="popular__img">
+
+                    <div class="popular__data">
+                        <h2 class="popular__price">
+                            <span>$</span>66,356
+                        </h2>
+                        <h3 class="popular__title">
+                            Garden City Assat
+                        </h3>
+                        <p class="popular__description">
+                            Street The Garden City of Miraflores, Lima - peru Av Sol #9876
+                        </p>
+                    </div>
+                </article>
+                <article class="popular__card">
+                    <img src="@/img/popular1.jpg" alt="" class="popular__img">
+
+                    <div class="popular__data">
+                        <h2 class="popular__price">
+                            <span>$</span>66,356
+                        </h2>
+                        <h3 class="popular__title">
+                            Garden City Assat
+                        </h3>
+                        <p class="popular__description">
+                            Street The Garden City of Miraflores, Lima - peru Av Sol #9876
+                        </p>
+                    </div>
+                </article>
+            </div>
+        </div>
     </div>
   </div>
 </template>
@@ -17,4 +97,14 @@ export default {};
 
 <style lang="scss" scoped>
 @import "@/scss/variables.scss";
+
+.residencies {
+  background: linear-gradient(170deg, 
+             hsl(0, 0%, 22%) 0%,   
+             hsl(0, 0%, 6%) 30%);
+  padding-bottom: 0;
+}
+
+
+
 </style>
