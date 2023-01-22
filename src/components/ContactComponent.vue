b9b5d5e6f089d817be4e0908529177af7e8544ba
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-22 14:59:48 -0700

    Starting to add style to the contact component

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -1,6 +1,82 @@
 <template>
-    <div>
+    <div class="contact section" ref="contact">
+        <div class="contact__container container grid">
+            <div class="contact__images">
+                <div class="contact__orbe"></div>
 
+                <div class="contact__img">
+                    <img src="@/img/contact.png" alt="">
+                </div>
+            </div>
+
+            <div class="contact__content">
+                <div class="contact__data">
+                    <span class="section__subtitle">Contact Us</span>
+                    <h2 class="section__title">
+                        Easy to Contact Us<span>.</span>
+                    </h2>
+                    <p class="contact__description">
+                        Is there a problem finding your dream home? Need a guide to buy your first home? Need a consultation about residential issues? Contact us now!
+                    </p>
+                </div>
+
+                <div class="contact__card">
+                    <div class="contact__card-box">
+                        <div class="contact__card-info">
+                            <i class='bx bxs-phone-call' ></i>
+                            <div>
+                                <h3 class="contact__card-title">
+                                    Call
+                                </h3>
+                                <p class="contact__card-description">
+                                    555-888-9999
+                                </p>
+                            </div>
+                        </div>
+
+                        <button class="button contact__card-button">
+                            Call Now
+                        </button>
+                    </div>
+
+                    <div class="contact__card-box">
+                        <div class="contact__card-info">
+                            <i class='bx bxs-message-rounded-dots' ></i>
+                            <div>
+                                <h3 class="contact__card_title">
+                                    Chat
+                                </h3>
+                                <p class="contact__card-description">
+                                    555-888-9999
+                                </p>
+                            </div>
+                        </div>
+
+                        <button class="button contact__card-button">
+                            Chat Now
+                        </button>
+                    </div>
+
+                    <div class="contact__card-box">
+                        <div class="contact__card-info">
+                            <i class='bx bxs-envelope' ></i>
+                            <div>
+                                <h3 class="contact__card_title">
+                                    Email
+                                </h3>
+                                <p class="contact__card-description">
+                                    real@estate.com
+                                </p>
+                            </div>
+                        </div>
+
+                        <button class="button contact__card-button">
+                            Email Us
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,5 +87,10 @@
 </script>
 
 <style lang="scss" scoped>
+.contact {
+    background: linear-gradient(170deg,
+                hsl(0, 0%, 22%) 0%,
+                hsl(0, 0%, 6%) 30%);
+}
 
 </style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-
+        <contact-component></contact-component>
     </div>
 </template>
 
 <script>
+import ContactComponent from '@/components/ContactComponent.vue'
     export default {
+  components: { ContactComponent },
         
     }
 </script>
