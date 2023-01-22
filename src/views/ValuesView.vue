7e945b1535004eaf661f691b124a6afe85b73f15
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-22 10:42:35 -0700

    Starting to add style to the value component

--- src/views/ValuesView.vue
+++ src/views/ValuesView.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-
+        <values-component></values-component>
     </div>
 </template>
 
 <script>
+import ValuesComponent from '@/components/ValuesComponent.vue'
     export default {
+  components: { ValuesComponent },
         
     }
 </script>
