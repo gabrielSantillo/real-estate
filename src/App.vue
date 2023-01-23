0be20a639b829b83253a64225c4285574684e176
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-23 12:07:35 -0700

    Starting to develop the footer component

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
   <div id="app">
     <header-component></header-component>
     <router-view/>
+    <footer-component></footer-component>
   </div>
 </template>
 
 <script>
+import FooterComponent from './components/FooterComponent.vue'
 import HeaderComponent from './components/HeaderComponent.vue'
     export default {
-  components: { HeaderComponent },
+  components: { HeaderComponent, FooterComponent },
         
     }
 </script>
