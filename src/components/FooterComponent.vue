3eae6454c080ee0eb0ffeeaad825bce84bc8a6be
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-23 13:13:42 -0700

    Starting to style the footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,102 @@
 <template>
-    <div>
+    <div class="footer section">
+        <div class="footer__container container grid">
+            <div>
+                <a href="" class="footer__logo">
+                    Real Estate <i class='bx bxs-home-alt-2' ></i>
+                </a>
 
+                <p class="footer__description">
+                    Our vision is to make all people <br>
+                    the best place to live for them.
+                </p>
+            </div>
+
+            <div class="footer__content">
+                <div>
+                    <h3 class="footer__title">
+                        About
+                    </h3>
+
+                    <ul class="footer__links">
+                        <li>
+                            <a href="" class="footer__link">About Us</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer__link">Features</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer__link">News & Blog</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 class="footer__title">
+                        Company
+                    </h3>
+
+                    <ul class="footer__links">
+                        <li>
+                            <a href="" class="footer__link">How we work?</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer__link">Capital</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer__link">Security</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 class="footer__title">
+                        Support
+                    </h3>
+
+                    <ul class="footer__links">
+                        <li>
+                            <a href="" class="footer__link">FAQs</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer__link">Support center</a>
+                        </li>
+                        <li>
+                            <a href="" class="footer__link">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3 class="footer__title">
+                        Follow me
+                    </h3>
+
+                    <ul class="footer__social">
+                        <li>
+                            <a href="https://www.linkedin.com/in/gabriel-santillo/" class="footer__social-link" target="_blank"><i class='bx bxl-linkedin-square' ></i></a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/gabrielSantillo" class="footer__social-link" target="_blank"><i class='bx bxl-github' ></i></a>
+                        </li>
+                        <li>
+                            <a href="https://gabrielsantillo.com/" class="footer__social-link" target="_blank">Portfolio</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer__info container">
+            <span class="footer__copy">
+                All rights reserved
+            </span>
+
+            <div class="footer__privacy">
+                <a href="">Terms & Agreements</a>
+                <a href="">Privacy Policy</a>
+            </div>
+        </div>
     </div>
 </template>
 
