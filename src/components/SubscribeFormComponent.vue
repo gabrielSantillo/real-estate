4dee747b28bad36356db94b9336af832970ecfdb
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-01-25 16:40:20 -0700

    Starting to add the subscribe form component

--- src/components/SubscribeFormComponent.vue
+++ src/components/SubscribeFormComponent.vue
@@ -1,6 +1,37 @@
 <template>
-    <div>
-        <h1>FORM</h1>
+    <div class="form">
+        <div class="form__content">
+            <img src="@/img/bg-form.png" alt="" class="form__img">
+
+            <form action="" class="subscribe__form">
+                <div>
+                    <h1 class="form__title">
+                        <span>Welcome</span> Back
+                    </h1>
+                    <p class="form__description">
+                        Subscribe to know more.
+                    </p>
+                </div>
+
+                <div>
+                    <div class="form__inputs">
+                        <div>
+                            <label for="" class="form__label">Email</label>
+                            <input type="email" required placeholder="Enter Your email address" class="form__input">
+                        </div>
+
+                        <div>
+                            <label for="" class="form__label">Full Name</label>
+                            <input type="text" required placeholder="Enter your full name" class="form__input">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form__buttons">
+                    <button class="form__button">Register</button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -11,5 +42,14 @@
 </script>
 
 <style lang="scss" scoped>
+@import '@/scss/variables.scss';
 
+.form {
+    background-color: $--first-color-light;
+    padding-top: 4.5rem;
+}
+
+.form__content, .subscribe__form, .form__inputs {
+    
+}
 </style>
--- src/views/SubscribeView.vue
+++ src/views/SubscribeView.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
-        
+        <subscribe-form-component></subscribe-form-component>
     </div>
 </template>
 
 <script>
+import SubscribeFormComponent from '@/components/SubscribeFormComponent.vue'
     export default {
+  components: { SubscribeFormComponent },
         
     }
 </script>
